21525e4a6b25e8084e2e98d116f981ffce8f8635
meya <yazganmelih@outlook.com>
2022-08-05 14:33:10 +0200

    Update Entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,19 +2,16 @@
 
 GH_OWNER=$GH_OWNER
 GH_REPOSITORY=$GH_REPOSITORY
+GH_TOKEN=$GH_TOKEN
 
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
-RUNNER_NAME="ephemeral-runner"
+RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
 
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-#GH_TOKEN = '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'
-
-#REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/enterprises/ENTERPRISE/actions/runners/registration-token | jq .token --raw-output)
-#REG_TOKEN = "AAAEF7UAWKUNEWVYVK5RFUTC5T7AA"
-#./config.sh --unattended --url https://github.boschdevcloud.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} --ephemeral
-./config.sh --unattended --url https://github.boschdevcloud.com/HydraulicHubPoC/test_ephemeral_runner --token AAAEF7RS6O7KQHZTUPKAX5LC5UBPY --name ${RUNNER_NAME} --ephemeral
+./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} --ephemeral
 
 cleanup() {
     echo "Removing runner..."
